89f2cd78142d005b28f4fead68709c0b17f5b8e5
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-11-22 23:07:12 -0800

    Add code to display form in the view and controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,28 @@
 class ImagesController < ApplicationController
   def new
+  	@image = Image.new
+  end
+
+  def create
+  	@image = Image.new image_params
+  	if @image.save
+  		redirect_to @image
+  	else
+  		render 'new'
+  	end
+  end
+
+  def index
+  	@images = Image.all
+  end
+
+  def show
+  	@image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+  	params.require(:image).permit(:title, :avatar)
   end
 end
